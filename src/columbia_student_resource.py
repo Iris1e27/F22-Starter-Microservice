89e4759db4f06e3cf3d16ddb67eafacbb733c805
Jane <42087697+Iris1e27@users.noreply.github.com>
2022-09-27 15:27:50 -0400

    Update columbia_student_resource.py

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -11,9 +11,9 @@ class ColumbiaStudentResource:
     @staticmethod
     def _get_connection():
 
-        usr = os.environ.get("DBUSER")
-        pw = os.environ.get("DBPW")
-        h = os.environ.get("DBHOST")
+        usr = "admin"
+        pw = "dbuserdbuser"
+        h = "e61561.cozl0ywa0hxk.us-east-1.rds.amazonaws.com"
 
         conn = pymysql.connect(
             user=usr,
